09d1465272beb93f01e671ce25bd1a83e1e764e2
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2021-01-08 11:20:44 +0000

    seperated out set up files and source into main

---
+++ scripts/libraries.R
@@ -0,0 +1,38 @@
+# libraries
+
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+
+if(!require(knitr)){
+  install.packages("knitr")
+  library(knitr)
+}
+
+if(!require(psych)){
+  install.packages("psych")
+  library(psych)
+}
+
+if(!require(polycor)){
+  install.packages("polycor")
+  library(polycor)
+}
+
+if(!require(corrplot)){
+  install.packages("corrplot")
+  library(corrplot)
+}
+
+if(!require(patchwork)){
+  install.packages("patchwork")
+  library(patchwork)
+}
+
+if(!require(ggpubr)){
+  install.packages("ggpubr")
+  library(ggpubr)
+}
+
---
+++ scripts/palettes.r
@@ -0,0 +1,54 @@
+## treatment paper colour palettes
+
+
+palette2 <- c("#78D9C5",
+              "#F5BE5E")
+
+palette3 <- c("#78D9C5",
+              "#F5BE5E",
+              "#EEB6E9")
+
+palette4 <- c("#78D9C5",
+              "#F5BE5E",
+              "#EEB6E9",
+              "#DBDB73")
+
+palette5 <- c("#78D9C5",
+              "#F5BE5E",
+              "#EEB6E9",
+              "#DBDB73",
+              "#FFED98")
+
+palette6 <- c("#78D9C5",
+              "#F5BE5E",
+              "#EEB6E9",
+              "#DBDB73",
+              "#FFED98",
+              "#BFD2EB")
+
+palette7 <- c("#78D9C5",
+              "#F5BE5E",
+              "#EEB6E9",
+              "#DBDB73",
+              "#FFED98",
+              "#BFD2EB",
+              "#808080")
+
+paletteGRAD <- c("#F5BE5E",
+                 "#FFD284",
+                 "#FFEED1",
+                 "#B5B5B5",
+                 "#DEFFF8",
+                 "#94F6E1",
+                 "#78D9C5")
+
+NeuCenterpalette <- c("#78D9C5",
+                      "#808080",
+                      "#F5BE5E")
+
+worseGRADpalette <- c("#78D9C5",
+                      "#FFEED1",
+                      "#F5BE5E",
+                      "#FFB1B5")
+GLADpalette = c("#efc00b",
+                "#b7dee8")
