52d5e83ed9a06baf972fd3dfae4b45f899a0f71e
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2021-01-19 14:55:21 +0000

    clarified barriers

--- scripts/Barriers_column_creation.R
+++ scripts/Barriers_column_creation.R
@@ -1,39 +1,44 @@
 ## create barrier to treatment supported vs client led columns
 
 treat_dat <- treat_dat %>%
-  mutate(PreventHelp_Supported  = case_when(preventhelp_emergency_mh == "Systemic" ~ "Systemic",
-                                            preventhelp_existing_mh_team == "Systemic" ~"Systemic",
-                                            preventhelp_online_talk_therapy == "Systemic" ~ "Systemic",
-                                            preventhelp_gp == "Systemic" ~ "Systemic",
-                                            preventhelp_nonnhs_phone == "Systemic" ~ "Systemic",
-                                            preventhelp_nhs_111 == "Systemic" ~ "Systemic",
-                                            
-                                            preventhelp_emergency_mh == "Client-level" ~ "Client-level",
-                                            preventhelp_existing_mh_team == "Client-level" ~"Client-level",
-                                            preventhelp_online_talk_therapy == "Client-level" ~ "Client-level",
-                                            preventhelp_gp == "Client-level" ~ "Client-level",
-                                            preventhelp_nonnhs_phone == "Client-level" ~ "Client-level",
-                                            preventhelp_nhs_111 == "Client-level" ~ "Client-level",
-                                            
-                                            preventhelp_emergency_mh == "Other" ~ "Other",
-                                            preventhelp_existing_mh_team == "Other" ~"Other",
-                                            preventhelp_online_talk_therapy == "Other" ~ "Other",
-                                            preventhelp_gp == "Other" ~ "Other",
-                                            preventhelp_nonnhs_phone == "Other" ~ "Other",
-                                            preventhelp_nhs_111 == "Other" ~ "Other"),
+  mutate(PreventHelp_Supported.Systemic  = case_when(preventhelp_emergency_mh == "Systemic" ~ 1,
+                                            preventhelp_existing_mh_team == "Systemic" ~1,
+                                            preventhelp_online_talk_therapy == "Systemic" ~ 1,
+                                            preventhelp_gp == "Systemic" ~ 1,
+                                            preventhelp_nonnhs_phone == "Systemic" ~ 1,
+                                            preventhelp_nhs_111 == "Systemic" ~ 1,
+                                            TRUE ~ 0),
+         PreventHelp_Supported.Client = case_when(preventhelp_emergency_mh == "Client-level" ~ 1,
+                                            preventhelp_existing_mh_team == "Client-level" ~1,
+                                            preventhelp_online_talk_therapy == "Client-level" ~ 1,
+                                            preventhelp_gp == "Client-level" ~ 1,
+                                            preventhelp_nonnhs_phone == "Client-level" ~ 1,
+                                            preventhelp_nhs_111 == "Client-level" ~ 1,
+                                            TRUE ~ 0),
+         PreventHelp_Supported.Other = case_when(
+                                            preventhelp_emergency_mh == "Other" ~ 1,
+                                            preventhelp_existing_mh_team == "Other" ~1,
+                                            preventhelp_online_talk_therapy == "Other" ~ 1,
+                                            preventhelp_gp == "Other" ~ 1,
+                                            preventhelp_nonnhs_phone == "Other" ~ 1,
+                                            preventhelp_nhs_111 == "Other" ~ 1,
+                                            TRUE ~ 0),
          
-         PreventHelp_SelfGuided  = case_when(preventhelp_gov_website == "Systemic" ~ "Systemic",
-                                             preventhelp_online_self_therapy == "Systemic" ~"Systemic",
-                                             preventhelp_struc_therapy == "Systemic" ~ "Systemic",
-                                             preventhelp_nongov_website == "Systemic" ~ "Systemic",
-                                             
-                                             preventhelp_gov_website == "Client-level" ~ "Client-level",
-                                             preventhelp_online_self_therapy == "Client-level" ~"Client-level",
-                                             preventhelp_struc_therapy == "Client-level" ~ "Client-level",
-                                             preventhelp_nongov_website == "Client-level" ~ "Client-level",
-                                             
-                                             preventhelp_gov_website == "Other" ~ "Other",
-                                             preventhelp_online_self_therapy == "Other" ~"Other",
-                                             preventhelp_struc_therapy == "Other" ~ "Other",
-                                             preventhelp_nongov_website == "Other" ~ "Other"))
+         PreventHelp_SelfGuided.Systemic  = case_when(preventhelp_gov_website == "Systemic" ~ 1,
+                                             preventhelp_online_self_therapy == "Systemic" ~1,
+                                             preventhelp_struc_therapy == "Systemic" ~ 1,
+                                             preventhelp_nongov_website == "Systemic" ~ 1,
+                                             TRUE ~0),
+         PreventHelp_SelfGuided.Client = case_when(
+                                             preventhelp_gov_website == "Client-level" ~ 1,
+                                             preventhelp_online_self_therapy == "Client-level" ~1,
+                                             preventhelp_struc_therapy == "Client-level" ~ 1,
+                                             preventhelp_nongov_website == "Client-level" ~ 1,
+                                             TRUE ~ 0),
+         PreventHelp_SelfGuided.Other =case_when(
+                                             preventhelp_gov_website == "Other" ~ 1,
+                                             preventhelp_online_self_therapy == "Other" ~1,
+                                             preventhelp_struc_therapy == "Other" ~ 1,
+                                             preventhelp_nongov_website == "Other" ~ 1,
+                                             TRUE ~ 0))
          
