b32513b25f5d7e25b85add32c30d13e908034b29
ks-young <katherine.s.young@kcl.ac.uk>
2021-12-23 18:24:34 +0000

    adding analyses based on age group; new script for age recoded to equal intervals; additional new script ready to use more updated data (need to pick timepoints post Jan 2021 that make sense to use

--- scripts/COPINGwaves.R
+++ scripts/COPINGwaves.R
@@ -35,6 +35,48 @@ end8 <-   as.POSIXct("2021-01-01")
 start9 <- as.POSIXct("2021-01-01")
 end9 <-   as.POSIXct("2021-02-01") 
 
+#February
+start10 <- as.POSIXct("2021-02-01")
+end10 <-   as.POSIXct("2021-03-01") 
+
+#March
+start11 <- as.POSIXct("2021-03-01")
+end11 <-   as.POSIXct("2021-04-01") 
+
+#April
+start12 <- as.POSIXct("2021-04-01")
+end12 <-   as.POSIXct("2021-05-01") 
+
+#May
+start13 <- as.POSIXct("2021-06-01")
+end13 <-   as.POSIXct("2021-07-01") 
+
+#June
+start14 <- as.POSIXct("2021-06-01")
+end14 <-   as.POSIXct("2021-07-01") 
+
+#July
+start15 <- as.POSIXct("2021-07-01")
+end15 <-   as.POSIXct("2021-08-01") 
+
+#August
+start16 <- as.POSIXct("2021-08-01")
+end16 <-   as.POSIXct("2021-09-01") 
+
+#September
+start17 <- as.POSIXct("2021-09-01")
+end17 <-   as.POSIXct("2021-10-01") 
+
+#October
+start18 <- as.POSIXct("2021-10-01")
+end18 <-   as.POSIXct("2021-11-01") 
+
+#November
+start19 <- as.POSIXct("2021-11-01")
+end19 <-   as.POSIXct("2021-12-01") 
+
+
+
 treatCOPE <- 
   treatCOPE %>%
   mutate(wave =  case_when(startDate >= start2 & startDate < end2 ~ "June 2020",
@@ -44,5 +86,15 @@ treatCOPE <-
                            startDate >= start6 & startDate < end6 ~ "October 2020",
                            startDate >= start7 & startDate < end7 ~ "November 2020",
                            startDate >= start8 & startDate < end8 ~ "December 2020",
-                           startDate >= start9 & startDate < end9 ~ "January 2021"))
+                           startDate >= start9 & startDate < end9 ~ "January 2021",
+                           startDate >= start10 & startDate < end10 ~ "February 2021",
+                           startDate >= start11 & startDate < end11 ~ "March 2021",
+                           startDate >= start12 & startDate < end12 ~ "April 2021",
+                           startDate >= start13 & startDate < end13 ~ "May 2021",
+                           startDate >= start14 & startDate < end14 ~ "June 2021",
+                           startDate >= start15 & startDate < end15 ~ "July 2021",
+                           startDate >= start16 & startDate < end16 ~ "August 2021",
+                           startDate >= start17 & startDate < end17 ~ "September 2021",
+                           startDate >= start18 & startDate < end18 ~ "October 2021",
+                           startDate >= start19 & startDate < end19 ~ "November 2021"))
 
